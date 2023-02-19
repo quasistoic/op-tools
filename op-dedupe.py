40b7a86e2ac62c0da9c13c10c64774d6ce8504a0
Danny Dawson <github@quasistoic.org>
2023-02-19 15:14:06 -0800

    Now useful for visually listing duplicates
    Still not useful for merging, but hey, this is better than before I had any code.

--- op-dedupe.py
+++ op-dedupe.py
@@ -9,6 +9,50 @@ import sys
 import tkinter as tk
 from tkinter import messagebox
 
+from urllib.parse import urlparse
+
+
+def get_domain_from_url(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    if domain.startswith('www.'):
+        domain = domain[4:]
+    return domain
+
+
+class ItemDetails:
+
+    def __init__(self, serialized):
+        self.id = None
+        self.serialized = serialized
+        self.domains = set([])
+        details = {}
+        for line in serialized.split("\n"):
+            items = line.strip().split(": ")
+            if len(items) >= 2:
+                key = items[0].strip()
+                values = []
+                for i in items[1:]:
+                    stripped = i.strip()
+                    if stripped != "http:// (primary)" and get_domain_from_url(stripped):
+                        self.domains.add(get_domain_from_url(stripped))
+                    values.append(stripped)
+                details[key] = values
+        self.fields = details
+        self.id = self.fields['ID'][0]
+        if self.domains:
+            logging.debug(f"Domains for {self.fields['ID']} : {self.domains}")
+
+    def __str__(self):
+        return str(sorted(self.fields.items()))
+
+    def is_duplicate(self, other):
+        return bool(self.get_shared_domains(other))
+
+    def get_shared_domains(self, other):
+        return self.domains & other.domains
+
+
 
 class OpTool:
     def __init__(self, vault):
@@ -18,7 +62,7 @@ class OpTool:
     def run_command(self, command):
         cache_file = f"./.op-cache/.{self.vault}.{command}.cache"
         if os.path.exists(cache_file):
-            logging.info(f"pulling from cache: {cache_file}")
+            logging.debug(f"pulling from cache: {cache_file}")
             with open(cache_file, "rb") as f:
                 return pickle.load(f)
 
@@ -37,14 +81,7 @@ class OpTool:
 
     def get_item_details(self, item):
         output = self.run_command(f"item get {item}")
-        details = {}
-        for line in output.split("\n"):
-            if line.startswith("      "):
-                items = line.strip().split(": ")
-                if len(items) >= 2:
-                    key, value = items[0], items[1]
-                    details[key] = value
-        return str(details)
+        return ItemDetails(output)
 
     def delete_item(self, item):
         self.run_command(f"delete item {item}")
@@ -54,9 +91,17 @@ class OpTool:
         duplicates = []
         for i, item in enumerate(self.items):
             details = self.get_item_details(item)
-            matching_items = [j for j in self.items[i+1:] if self.get_item_details(j) == details and j != item]
-            if matching_items:
-                duplicates.append([item] + matching_items)
+            if details:
+                matching_items = []
+                for j in self.items[i+1:]:
+                    j_details = self.get_item_details(j)
+                    if j_details.is_duplicate(details) and str(j_details) != str(details):
+                        matching_items.append(j_details)
+                if matching_items:
+                    logging.debug(f"Found duplicates: {item}\n{matching_items}")
+                    duplicates.append([details] + matching_items)
+
+        logging.info(f"Found {len(duplicates)} sets of duplicates.")
         return duplicates
 
     def show_duplicate_manager(self):
@@ -65,21 +110,22 @@ class OpTool:
             messagebox.showinfo('No Duplicates Found', 'No duplicate items were found.')
             return
 
-        logging.info("Found the following duplicates: {}", duplicates)
+        logging.debug("Found the following duplicates: {}", duplicates)
 
         def apply_changes():
-            for i, duplicate in enumerate(duplicates):
+            for i, duplicate_set in enumerate(duplicates):
                 canonical_var = root.winfo_children()[i*2].winfo_children()[0].var
                 archive_var = root.winfo_children()[i*2].winfo_children()[1].var
                 merge_var = root.winfo_children()[i*2].winfo_children()[2].var
                 if canonical_var.get():
                     continue
-                for j in range(1, len(duplicate)):
+                for j in range(1, len(duplicate_set)):
                     if archive_var.get():
-                        logging.warning(['op', 'archive', 'item', duplicate[j]], file=sys.stderr)
+                        logging.warning(['op', 'archive', 'item', duplicate_set[j].id], file=sys.stderr)
                     if merge_var.get():
-                        logging.warning(['op', 'edit', 'item', duplicate[0], 'set', 'details', self.get_item_details(duplicate[j])], file=sys.stderr)
-                        logging.warning(['op', 'delete', 'item', duplicate[j]], file=sys.stderr)
+                        logging.warning(['op', 'edit', 'item', duplicate_set[0].id, 'set', 'details',
+                            self.get_item_details(duplicate_set[j].id)], file=sys.stderr)
+                        logging.warning(['op', 'delete', 'item', duplicate_set[j].id], file=sys.stderr)
 
         def display_duplicate_set(i, root):
             nonlocal duplicates
@@ -90,12 +136,12 @@ class OpTool:
             label.pack()
 
             duplicate = duplicates[i]
-            items = [self.get_item_details(item) for item in duplicate]
+            items = [self.get_item_details(item.id) for item in duplicate]
 
             listbox = tk.Listbox(root, height=len(duplicate), selectmode='single')
             listbox.pack(fill='both', expand=True)
             for j, item_details in enumerate(items):
-                listbox.insert(j, item_details)
+                listbox.insert(j, item_details.serialized)
 
             def on_select(event):
                 selection = listbox.curselection()
@@ -105,7 +151,7 @@ class OpTool:
                     for j, item in enumerate(duplicate):
                         if j != index:
                             logging.info(f"Editing item {item}")
-                            logging.warning(['op', 'edit', 'item', item, 'set', 'details', self.get_item_details(canonical_item)])
+                            logging.warning(['op', 'edit', 'item', item, 'set', 'details', self.get_item_details(canonical_item.id)])
                             logging.warning(['op', 'delete', 'item', item])
 
             listbox.bind('<<ListboxSelect>>', on_select)
@@ -119,13 +165,13 @@ class OpTool:
         label = tk.Label(root, text='Select a set of duplicates to manage:')
         label.pack()
         for i, duplicate in enumerate(duplicates):
-            button = tk.Button(root, text=duplicate[0], command=lambda i=i: display_duplicate_set(i, root))
+            button = tk.Button(root, text=duplicate[0].get_shared_domains(duplicate[1]), command=lambda i=i: display_duplicate_set(i, root))
             button.pack()
         root.mainloop()
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s', stream=sys.stderr)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s', stream=sys.stderr)
 
     if len(sys.argv) != 2:
         print("Usage: python op-dedupe.py VAULT_NAME")
@@ -134,12 +180,6 @@ def main():
     vault = sys.argv[1]
     tool = OpTool(vault)
     tool.get_items()
-
-    duplicates = tool.find_duplicates()
-    if not duplicates:
-        print("No duplicate items found.")
-        return
-
     tool.show_duplicate_manager()
 
 
