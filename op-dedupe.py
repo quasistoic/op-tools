7df885911108ba05a5f69879dbd491554bd112fa
Danny Dawson <github@quasistoic.org>
2023-02-19 10:07:17 -0800

    ChatGPT OO update
    Second version generated by ChatGPT in response to
    
    Please rewrite that in an object-oriented style.

--- op-dedupe.py
+++ op-dedupe.py
@@ -1,73 +1,75 @@
 import subprocess
 import tkinter as tk
 
-# Function to get a list of all 1Password items
-def get_items():
-    result = subprocess.run(['op', 'list', 'items'], capture_output=True)
-    items = result.stdout.decode('utf-8').strip().split('\n')
-    return items
 
-# Function to get the details of a 1Password item
-def get_item_details(uuid):
-    result = subprocess.run(['op', 'get', 'item', uuid], capture_output=True)
-    details = result.stdout.decode('utf-8')
-    return details
+class OnePasswordTool:
+    def __init__(self):
+        self.items = self.get_items()
 
-# Function to search for duplicate items
-def find_duplicates():
-    items = get_items()
-    duplicates = []
-    for item in items:
-        details = get_item_details(item)
-        if details in duplicates:
-            continue
-        matching_items = [i for i in items if i != item and get_item_details(i) == details]
-        if matching_items:
-            duplicates.append(details)
-            duplicates.extend(matching_items)
-    return duplicates
+    def get_items(self):
+        result = subprocess.run(['op', 'list', 'items'], capture_output=True)
+        items = result.stdout.decode('utf-8').strip().split('\n')
+        return items
 
-# Function to display the GUI for managing duplicates
-def show_duplicate_manager():
-    duplicates = find_duplicates()
-    if not duplicates:
-        tk.messagebox.showinfo('No Duplicates Found', 'No duplicate items were found.')
-        return
-    root = tk.Tk()
-    root.title('1Password Duplicate Manager')
-    label = tk.Label(root, text='Select the canonical item for each set of duplicates:')
-    label.pack()
-    for i, duplicate in enumerate(duplicates):
-        canonical_var = tk.BooleanVar()
-        canonical_var.set(i == 0)
-        archive_var = tk.BooleanVar()
-        merge_var = tk.BooleanVar()
-        frame = tk.Frame(root)
-        frame.pack()
-        canonical_checkbutton = tk.Checkbutton(frame, text='Canonical', variable=canonical_var)
-        archive_checkbutton = tk.Checkbutton(frame, text='Archive Non-Canonical', variable=archive_var)
-        merge_checkbutton = tk.Checkbutton(frame, text='Merge Fields', variable=merge_var)
-        canonical_checkbutton.pack(side='left')
-        archive_checkbutton.pack(side='left')
-        merge_checkbutton.pack(side='left')
-        label = tk.Label(root, text=duplicate, justify='left')
+    def get_item_details(self, uuid):
+        result = subprocess.run(['op', 'get', 'item', uuid], capture_output=True)
+        details = result.stdout.decode('utf-8')
+        return details
+
+    def find_duplicates(self):
+        duplicates = []
+        for i, item in enumerate(self.items):
+            details = self.get_item_details(item)
+            if details in duplicates:
+                continue
+            matching_items = [i for i in self.items[i+1:] if self.get_item_details(i) == details]
+            if matching_items:
+                duplicates.append(details)
+                duplicates.append([item] + matching_items)
+        return duplicates
+
+    def show_duplicate_manager(self):
+        duplicates = self.find_duplicates()
+        if not duplicates:
+            tk.messagebox.showinfo('No Duplicates Found', 'No duplicate items were found.')
+            return
+        root = tk.Tk()
+        root.title('1Password Duplicate Manager')
+        label = tk.Label(root, text='Select the canonical item for each set of duplicates:')
         label.pack()
-    button = tk.Button(root, text='Apply Changes', command=root.quit)
-    button.pack()
-    root.mainloop()
-    # Apply the changes
-    for i, duplicate in enumerate(duplicates):
-        canonical_var = root.winfo_children()[i*2].winfo_children()[0].var
-        archive_var = root.winfo_children()[i*2].winfo_children()[1].var
-        merge_var = root.winfo_children()[i*2].winfo_children()[2].var
-        if canonical_var.get():
-            continue
-        for j in range(1, len(duplicate)):
-            if archive_var.get():
-                subprocess.run(['op', 'archive', 'item', duplicate[j]])
-            if merge_var.get():
-                subprocess.run(['op', 'edit', 'item', duplicate[0], 'set', 'details', get_item_details(duplicate[j])])
-                subprocess.run(['op', 'delete', 'item', duplicate[j]])
+        for i, duplicate in enumerate(duplicates):
+            canonical_var = tk.BooleanVar()
+            canonical_var.set(i == 0)
+            archive_var = tk.BooleanVar()
+            merge_var = tk.BooleanVar()
+            frame = tk.Frame(root)
+            frame.pack()
+            canonical_checkbutton = tk.Checkbutton(frame, text='Canonical', variable=canonical_var)
+            archive_checkbutton = tk.Checkbutton(frame, text='Archive Non-Canonical', variable=archive_var)
+            merge_checkbutton = tk.Checkbutton(frame, text='Merge Fields', variable=merge_var)
+            canonical_checkbutton.pack(side='left')
+            archive_checkbutton.pack(side='left')
+            merge_checkbutton.pack(side='left')
+            label = tk.Label(root, text=duplicate[0], justify='left')
+            label.pack()
+        button = tk.Button(root, text='Apply Changes', command=root.quit)
+        button.pack()
+        root.mainloop()
+        # Apply the changes
+        for i, duplicate in enumerate(duplicates):
+            canonical_var = root.winfo_children()[i*2].winfo_children()[0].var
+            archive_var = root.winfo_children()[i*2].winfo_children()[1].var
+            merge_var = root.winfo_children()[i*2].winfo_children()[2].var
+            if canonical_var.get():
+                continue
+            for j in range(1, len(duplicate)):
+                if archive_var.get():
+                    subprocess.run(['op', 'archive', 'item', duplicate[j]])
+                if merge_var.get():
+                    subprocess.run(['op', 'edit', 'item', duplicate[0], 'set', 'details', self.get_item_details(duplicate[j])])
+                    subprocess.run(['op', 'delete', 'item', duplicate[j]])
+
 
 # Example usage
-show_duplicate_manager()
+tool = OnePasswordTool()
+tool.show_duplicate_manager()
