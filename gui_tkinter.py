1507536b4b8fc5cdcebf94101687c684f0c4550e
Danny Dawson <github@quasistoic.org>
2023-03-02 16:05:18 -0700

    Finish splitting GUI code out of main file

--- gui_tkinter.py
+++ gui_tkinter.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 import logging
-import sys
 import tkinter as tk
 from tkinter import messagebox
 
 import op_api
 
 
-class OpToolUI:
+class TkinterGUI:
     """Controller for the Tkinter Duplicate Manager GUI."""
 
     def __init__(self, vault):
@@ -197,20 +196,3 @@ class OpToolUI:
             button.pack()
 
         self.root.mainloop()
-
-
-def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s:%(levelname)s:%(message)s',
-        stream=sys.stderr)
-
-    vault = None
-    if len(sys.argv) >= 2:
-        vault = sys.argv[1]
-    tool = OpToolUI(vault)
-    tool.run()
-
-
-if __name__ == "__main__":
-    main()
